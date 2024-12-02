01dd6856a6ea53eec2e08fab3a7bd7a83f917a48
kilee-77 <kwangin77@gmail.com>
2024-12-02 13:16:13 +0900

    Create masterkey.sh

---
+++ masterkey.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#token key 
+sudo kubeadm token list
+
+#sha256 key
+sudo openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'
