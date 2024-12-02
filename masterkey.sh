0154bb671c3fb986f14bba4801d9e7fc71a7c3c5
kilee-77 <kwangin77@gmail.com>
2024-12-02 13:45:12 +0900

    Update masterkey.sh

--- masterkey.sh
+++ masterkey.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #token key 
+echo token key:
 sudo kubeadm token list
-
+echo
+echo sha256 key:
 #sha256 key
 sudo openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'
